496d64a044c71a3bdb2a7fc3390612a86b7af41d
Jake Wood <jake@jakewood.org>
2020-09-29 23:51:57 -0700

    SMS receive works

--- app/src/main/java/org/jakewood/z2fa_receiver/SmsReceiver.kt
+++ app/src/main/java/org/jakewood/z2fa_receiver/SmsReceiver.kt
@@ -3,12 +3,32 @@ package org.jakewood.z2fa_receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.Bundle
 import android.provider.Telephony
+import android.telephony.SmsMessage
 
 class SmsReceiver : BroadcastReceiver() {
+    private fun getIncomingMessage(obj: ByteArray, bundle: Bundle): SmsMessage {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val format = bundle.getString("format")
+            SmsMessage.createFromPdu(obj, format)
+        } else {
+            SmsMessage.createFromPdu(obj)
+        }
+    }
     override fun onReceive(ctx: Context?, intent: Intent?) {
         if (intent != null && intent.action == Telephony.Sms.Intents.SMS_RECEIVED_ACTION) {
-
+            val bundle = intent.extras
+            if (bundle != null) {
+                val pduObjs = bundle.get("pdus") as? Array<ByteArray>
+                if (pduObjs != null) {
+                    for (pdu in pduObjs) {
+                        val sms = getIncomingMessage(pdu, bundle)
+                        println("Body is " + sms.displayMessageBody)
+                    }
+                }
+            }
         }
     }
 }
